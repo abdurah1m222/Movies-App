6c5341b7c07963fccf206c6417da1d49bc020895
Abdurahim <abdy42796@gmail.com>
2022-12-05 17:19:09 +0600

    Dependency injection setup

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.movies
-//https://rutracker.org/forum/viewtopic.php?t=6240289
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.movies.navigation.SetupNavHost
 import com.example.movies.ui.theme.MoviesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
